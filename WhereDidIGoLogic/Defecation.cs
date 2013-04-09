1cdf0c809fb75df3f27934bfe3b0adb315f5034a
Jeff Blankenburg <github@jeffblankenburg.com>
2013-04-08 20:40:58 -0400

    Initial Commit

---
+++ WhereDidIGoLogic/Defecation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhereDidIGoLogic
+{
+    public class Defecation : Elimination
+    {
+        public int BristolStoolScaleRating { get; set; }
+    }
+}
---
+++ WhereDidIGoLogic/Elimination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhereDidIGoLogic
+{
+    public class Elimination
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Type { get; set; }
+        public string ImageLocation { get; set; }
+        public int Volume { get; set; }
+        public int Discomfort { get; set; }
+    }
+}
